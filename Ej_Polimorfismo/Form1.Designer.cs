6931c81ac31560842a8dfc49a46c50340449b26c
Emanuel12 <cordobaemanuel307@gmail.com>
2023-06-03 19:18:47 -0300

    Agregar archivos de proyecto.

---
+++ Ej_Polimorfismo/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace Ej_Polimorfismo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.buttonAgregar = new System.Windows.Forms.Button();
+            this.radioButtonEmplAdministrativo = new System.Windows.Forms.RadioButton();
+            this.radioButtonEmplGerente = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 222);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // buttonAgregar
+            // 
+            this.buttonAgregar.Location = new System.Drawing.Point(13, 261);
+            this.buttonAgregar.Name = "buttonAgregar";
+            this.buttonAgregar.Size = new System.Drawing.Size(75, 23);
+            this.buttonAgregar.TabIndex = 1;
+            this.buttonAgregar.Text = "Agregar";
+            this.buttonAgregar.UseVisualStyleBackColor = true;
+            this.buttonAgregar.Click += new System.EventHandler(this.buttonAgregar_Click);
+            // 
+            // radioButtonEmplAdministrativo
+            // 
+            this.radioButtonEmplAdministrativo.AutoSize = true;
+            this.radioButtonEmplAdministrativo.Checked = true;
+            this.radioButtonEmplAdministrativo.Location = new System.Drawing.Point(228, 266);
+            this.radioButtonEmplAdministrativo.Name = "radioButtonEmplAdministrativo";
+            this.radioButtonEmplAdministrativo.Size = new System.Drawing.Size(140, 17);
+            this.radioButtonEmplAdministrativo.TabIndex = 2;
+            this.radioButtonEmplAdministrativo.TabStop = true;
+            this.radioButtonEmplAdministrativo.Text = "Empelado Administrativo";
+            this.radioButtonEmplAdministrativo.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonEmplGerente
+            // 
+            this.radioButtonEmplGerente.AutoSize = true;
+            this.radioButtonEmplGerente.Location = new System.Drawing.Point(228, 302);
+            this.radioButtonEmplGerente.Name = "radioButtonEmplGerente";
+            this.radioButtonEmplGerente.Size = new System.Drawing.Size(113, 17);
+            this.radioButtonEmplGerente.TabIndex = 3;
+            this.radioButtonEmplGerente.Text = "Empleado Gerente";
+            this.radioButtonEmplGerente.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.radioButtonEmplGerente);
+            this.Controls.Add(this.radioButtonEmplAdministrativo);
+            this.Controls.Add(this.buttonAgregar);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button buttonAgregar;
+        private System.Windows.Forms.RadioButton radioButtonEmplAdministrativo;
+        private System.Windows.Forms.RadioButton radioButtonEmplGerente;
+    }
+}
+
---
+++ Ej_Polimorfismo/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace Ej_Polimorfismo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<Empleado> le;
+
+        private void buttonAgregar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Empleado auxE = null;
+                if (radioButtonEmplAdministrativo.Checked)
+                {
+                    auxE = new EmpleadoAdministrativo(Interaction.InputBox("Nombre : "), Interaction.InputBox("DNI : "), Interaction.InputBox("Apellido : "), decimal.Parse(Interaction.InputBox("Suledo Bruto")), decimal.Parse(Interaction.InputBox("Premio : ")));
+                }
+                else
+                {
+                    auxE = new EmpleadoGerente(Interaction.InputBox("Nombre : "), Interaction.InputBox("DNI : "), Interaction.InputBox("Apellido : "), decimal.Parse(Interaction.InputBox("Suledo Bruto")), decimal.Parse(Interaction.InputBox("Bono : ")));
+                }
+
+                le.Add(auxE);
+                Mostrar(dataGridView1, le);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            
+        }
+
+        private void Mostrar(DataGridView dDGV,List<Empleado> pListaEmpleado)
+        {
+            EmpleadoVista ev = new EmpleadoVista();
+
+            dDGV.DataSource = null;
+
+            dDGV.DataSource = ev.RetornaListadeEmpleado(pListaEmpleado);
+
+        }
+
+        public class EmpleadoVista
+        {
+            public EmpleadoVista(string pNombre, string pApellido, string pDNI, decimal pSueldoBruto, decimal pDescuento, decimal pPremio, decimal pBono, decimal pSueldoApagar )
+            {
+                Nombre = pNombre; Apellido = pApellido; DNI = pDNI; SueldoBruto = pSueldoBruto; Descuento = pDescuento; Premio = pPremio; Bono = pBono ; SueldoApagar = pSueldoApagar;
+            }
+
+            public EmpleadoVista() { }
+
+            public string Nombre { get; set; }
+
+            public string Apellido { get; set; }
+
+            public string DNI { get; set; }
+
+            public decimal SueldoBruto { get; set; }
+
+            public decimal Descuento { get; set; }
+
+            public decimal Premio { get; set; }
+
+            public decimal Bono { get; set; }
+
+            public decimal SueldoApagar { get; set; }
+
+            public List<EmpleadoVista> RetornaListadeEmpleado(List<Empleado> pListaDeEmpleado)
+            {
+                List<EmpleadoVista> auxLEV = new List<EmpleadoVista>();
+
+                foreach(Empleado emp in pListaDeEmpleado)
+                {
+                    EmpleadoVista auxEV = new EmpleadoVista();
+
+                    auxEV.Nombre = emp.Nombre;
+
+                    auxEV.DNI = emp.DNI;
+
+                    auxEV.Apellido = emp.Apellido;
+
+                    auxEV.SueldoBruto = emp.SueldoBruto;
+
+                    auxEV.SueldoApagar = emp.CalcularSueldo();
+
+                    auxEV.Descuento = emp.Descuento;
+
+                    if (emp is EmpleadoAdministrativo)
+                    {
+                        auxEV.Premio = (emp as EmpleadoAdministrativo).Premio;
+                        auxEV.Bono =0;
+                    }
+                    else
+                    {
+                        auxEV.Bono = (emp as EmpleadoGerente).BonoPorciento;
+                        auxEV.Premio = 0;
+                    }
+                    auxLEV.Add(auxEV);
+                }
+
+                return auxLEV;
+            }
+
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            le = new List<Empleado>();
+        }
+    }
+
+    public abstract class Empleado
+    {
+        public Empleado(string pNombre, string pDNI, string pApellido, decimal pSueldoBruto )
+        {
+            DNI = pDNI; Nombre = pNombre; Apellido = pApellido; SueldoBruto = pSueldoBruto;
+        }
+
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
+
+        public string DNI { get; set; }
+
+        public decimal SueldoBruto { get; set; }
+
+        public abstract decimal Descuento { get; }
+
+        public abstract decimal CalcularSueldo();
+    }
+
+    public class EmpleadoAdministrativo : Empleado
+    {
+        public EmpleadoAdministrativo(string pNombre, string pDNI, string pApellido, decimal pSueldoBruto, decimal pPremio):base(pNombre,pDNI,pApellido,pSueldoBruto)
+        {
+            Premio = pPremio;
+        }
+
+        public decimal Premio { get; set; }
+
+        decimal descuento;
+
+        public override decimal Descuento => descuento;
+
+        public override decimal CalcularSueldo()
+        {
+            descuento = (SueldoBruto + Premio) * .1m;
+
+            return (SueldoBruto + Premio - descuento);
+        }
+    }
+
+    public class EmpleadoGerente : Empleado
+    {
+        public EmpleadoGerente(string pNombre, string pDNI, string pApellido, decimal pSueldoBruto, decimal pBonoPorciento) : base(pNombre, pDNI, pApellido, pSueldoBruto)
+        {
+            BonoPorciento = pBonoPorciento;
+        }
+
+
+        public decimal BonoPorciento { get; set; }
+
+        decimal descuento;
+
+        public override decimal Descuento => descuento;
+
+        public override decimal CalcularSueldo()
+        {
+            descuento = (SueldoBruto * (1 + (BonoPorciento/100))) * .2m;
+
+            return (SueldoBruto * (1 + (BonoPorciento / 100))) - descuento;
+        }
+        
+    }
+}
